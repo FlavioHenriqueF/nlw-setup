afc07ea8f87bca90932756762e509feaddbd8a3b
Flavio Henrique Franco <flavio3224henrique@gmail.com>
2023-01-20 15:34:38 -0300

    Nlw Setup: Aula 04. Conectando a API

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -5,12 +5,14 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
 import { BackButton } from "../components/BackButton";
 import { CheckBox } from "../components/CheckBox";
 import colors from "tailwindcss/colors";
+import { api } from "../lib/axios";
 
 const avaiableWeekDays = [
   "Domingo",
@@ -24,6 +26,7 @@ const avaiableWeekDays = [
 
 export function New() {
   const [weekDays, setWeekDays] = useState<number[]>([]);
+  const [title, setTitle] = useState("");
 
   function handleToggleWeekDays(weekDaysIndex: number) {
     if (weekDays.includes(weekDaysIndex)) {
@@ -32,6 +35,27 @@ export function New() {
       );
     } else {
       setWeekDays((prevState) => [...prevState, weekDaysIndex]);
+    }
+  }
+
+  async function handleCreateNewHabit() {
+    try {
+      if (!title.trim() || weekDays.length === 0) {
+        Alert.alert(
+          "Novo Hábito",
+          "Informe o nome do hábito e escolha a periodicidade."
+        );
+      }
+
+      await api.post("/habits", { title, weekDays });
+
+      setTitle("");
+      setWeekDays([]);
+
+      Alert.alert("Novo Hábito", "Hábito criado com sucesso!");
+    } catch (e) {
+      console.log(e);
+      Alert.alert("Ops", "Não foi possivel criar o novo Hábito");
     }
   }
 
@@ -55,6 +79,8 @@ export function New() {
           className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
           placeholder="Exercícios, dormir bem, etc..."
           placeholderTextColor={colors.zinc[400]}
+          onChangeText={setTitle}
+          value={title}
         />
 
         <Text className="font-semibold mt-4 mb-3 text-white text-base">
@@ -73,6 +99,7 @@ export function New() {
         <TouchableOpacity
           className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
           activeOpacity={0.7}
+          onPress={handleCreateNewHabit}
         >
           <Feather name="check" size={20} color={colors.white} />
 
